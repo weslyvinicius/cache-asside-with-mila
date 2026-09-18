e83f5aa39547ad269dace62f57a523bff708d682
Camila Kfouri <kfouri.camila@gmail.com>
2026-09-18 09:43:50 +0100

    feat: add category entity and repository

---
+++ src/main/java/com/academy/cacheasside/entity/Category.java
@@ -0,0 +1,22 @@
+package com.academy.cacheasside.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
